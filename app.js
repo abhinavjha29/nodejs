60f074b7e9a9ddaa7299702d88952784b95c1ea9
abhinavjha29 <abhinav_29@outlook.com>
2023-05-16 21:57:13 +0530

    url routing methods

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 //new file
 const http = require("http") ;
 const server = http.createServer((req , res)=>{
+    const url = req.url ;
+    if(url==='/home') {
      res.write("<html>") ;
      res.write("<head><title>My first join</title></head>") ;
-     res.write("<body>Hello to my node world</body>") ;
+     res.write("<body>Welcome Home</body>") ;
      res.write("</html>") ;
-     res.end() ;
+     return res.end() ;
+    }
+    if(url==='/about') {
+        res.write("<html>") ;
+        res.write("<head><title>about us </title></head>") ;
+        res.write("<body> welcome to about us page</body>") ;
+        res.write("</html>") ;
+        return res.end() ;
+    }
+    if(url==='/node') {
+        res.write("<html>") ;
+        res.write("<head><title>project </title></head>") ;
+        res.write("<body> welcome to my node js project</body>") ;
+        res.write("</html>") ;
+        return res.end() ;
+    }
 }) ;
 server.listen(4000) ;
 
